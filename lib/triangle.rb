52e6a010bfebcf21a8753e45b422f8a5c3bff29a
Andrew Bernier <a6bernier@gmail.com>
2018-10-23 14:14:26 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,3 @@
-
 class Triangle
   attr_accessor :side1, :side2, :side3
 
@@ -9,28 +8,27 @@ class Triangle
   end
 
   def kind
-    if @side1 == @side2 && @side1 == @side3
-      :equilateral
-    elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
-      :isosceles
-    elsif @side1 != @side2 && @side1 != @side3
-      :scalene
-    end
+    if valid?
+      if @side1 == @side2 && @side2 == @side3
+        :equilateral
+      elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+        :isosceles
+      else
+        :scalene
+      end
     else
       raise TriangleError
     end
   end
 
-  def validate
-    @side1 + @side2 > @side3 && @side1 + @side3 > @side2 && @side2 + @side3 > @side1 && @side1 > 0 && @side2 > 0 && @side3 > 0
+  def valid?
+    side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1 && side1 > 0 && side2 > 0 && side3 > 0
   end
 
   class TriangleError < StandardError
-    # def message
-    #   "Sides provided do not make a valid triangle"
-    # end
+    def message
+      "Sides provided do not make a valid triangle."
+    end
   end
 
-
-end
 end
