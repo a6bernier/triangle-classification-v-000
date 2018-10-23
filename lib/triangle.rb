397697dd7e9521325a636f8db83a14232cab94e3
Andrew Bernier <a6bernier@gmail.com>
2018-10-23 14:13:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,6 +19,7 @@ class Triangle
     else
       raise TriangleError
     end
+  end
 
   def validate
     @side1 + @side2 > @side3 && @side1 + @side3 > @side2 && @side2 + @side3 > @side1 && @side1 > 0 && @side2 > 0 && @side3 > 0
