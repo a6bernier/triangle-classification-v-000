686607016a8daad18fd50e54747e36d2e0ebc8fd
Andrew Bernier <a6bernier@gmail.com>
2018-10-23 14:12:11 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,7 +21,7 @@ class Triangle
     end
 
   def validate
-    @side1 + @side2 > @side3 && @side1 + @side3 > @side2 && @side2 + @side3 > @side1 && @side1 > 0 && @side2 > 0 && @side 3 > 0
+    @side1 + @side2 > @side3 && @side1 + @side3 > @side2 && @side2 + @side3 > @side1 && @side1 > 0 && @side2 > 0 && @side3 > 0
 
   end
 
