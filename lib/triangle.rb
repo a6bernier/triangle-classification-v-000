d2de2573ff593b46b9fdbf9c7fe4cd2a4e2dd5fb
Andrew Bernier <a6bernier@gmail.com>
2018-10-23 14:13:42 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,6 @@ class Triangle
 
   def validate
     @side1 + @side2 > @side3 && @side1 + @side3 > @side2 && @side2 + @side3 > @side1 && @side1 > 0 && @side2 > 0 && @side3 > 0
-
   end
 
   class TriangleError < StandardError
